6efd42c7faa5455706aeb50f5f76f4dc81f30f6a
rishabhdoshi09 <rishabhdrubi@gmail.com>
2026-07-31 00:27:59 +0530

    chore(pages): remove duplicate Help entry point

--- pages/9_Help.py
+++
@@ -1,9 +0,0 @@
-"""Retail help entry point."""
-from __future__ import annotations
-
-import streamlit as st
-
-from ui.retail_help import render_retail_help
-
-st.set_page_config(page_title="Help | QuantTerm", page_icon="❓", layout="wide")
-render_retail_help()
