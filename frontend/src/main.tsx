9a97e7688a77df2b94b30c57d06bb5f3aff9efa9
rishabhdoshi09 <rishabhdrubi@gmail.com>
2026-07-31 23:52:29 +0530

    Load market operations workspace styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './truth.css'
 import './views.css'
+import './marketViews.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
