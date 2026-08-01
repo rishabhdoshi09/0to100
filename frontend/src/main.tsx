132505ce2c7817fc50431d5a297d48fa47c7f384
rishabhdoshi09 <rishabhdrubi@gmail.com>
2026-08-01 10:56:29 +0530

    Load product hardening styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 import './truth.css'
 import './views.css'
 import './marketViews.css'
+import './productViews.css'
 import './researchData.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
