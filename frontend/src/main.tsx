8ad7dd724835b955ce067f59fc48b56b088d75c5
rishabhdoshi09 <rishabhdrubi@gmail.com>
2026-09-17 22:54:54 +0530

    ui: load light-only theme last

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,6 +17,7 @@ import './operator-polish.css'
 import './home-calm.css'
 import './clarity.css'
 import './final-polish.css'
+import './light-only.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
